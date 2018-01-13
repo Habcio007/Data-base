df56287671f363b17840ae4086bd81f3846a7f82
Habcio007 <33881111+Habcio007@users.noreply.github.com>
2018-01-13 20:37:57 +0100

    Update Source.c

--- Source.c
+++ Source.c
@@ -1,9 +1,11 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
+#include<string.h>
 
 typedef struct Shop
 {
+	int add_order;
 	int number;
 	int amount;
 	float price;
@@ -13,25 +15,33 @@ typedef struct Shop
 	struct Shop *prev;
 }Shop;
 
+
+void add_any_place(Shop *a);
 void add();
 void add_file( int, float, long int, char *);
 void show();
 void save();
 void load();
+void order_number();
 void end();
 void delete_one(Shop *temp);
+void delete_all();
 void show_one();
+void swap(Shop *temp);
+void show_series(float data);
+void order_change();
 
-Shop *show_elem(char *);
+Shop *show_elem(float);
+Shop *show_char(char *data);
 Shop *first = NULL;
 Shop *last = NULL;
 
 void menu_main();
+void delete_things(Shop *, Shop *);
 
 int main()
 {
-	int opcja = 3;
-	int a;
+	show();
 	/*
 	do
 	{
@@ -73,14 +83,16 @@ int main()
 	} while (opcja);
 	*/
 	load();
+	order_number();
 	show();
-
-	scanf("%f", &a);
-	printf("%d;%d;%f;%d;%s\n", show_elem(a)->number, show_elem(a)->amount, show_elem(a)->price, show_elem(a)->bar_code, show_elem(a)->name);
+	delete_all();
+	printf("\n");
+	show();
 	system("pause");
 	return 0;
 
 }
+
 void add()
 {
 	Shop *new;
@@ -96,7 +108,7 @@ void add()
 	scanf("%s", new->name);
 	if (first == NULL)
 	{
-		new->number = 1;
+		new->add_order = 1;
 		new->next = NULL;
 		new->prev = NULL;
 		first = new;
@@ -104,11 +116,85 @@ void add()
 	}
 	else
 	{
-		new->number = last->number + 1;
+		new->add_order = last->add_order + 1;
 		new->next = NULL;
 		new->prev = last;
 		last->next = new;
 		last = new;
+	}
+}
+void add_any_place(Shop *a) {
+	Shop *new;
+	Shop *temp;
+	if(a==NULL){
+		return;
+	}
+	temp = a->next;
+	new = (Shop *)malloc(sizeof(Shop));
+	new->name = (char *)malloc(100 * sizeof(char));
+	printf("Podaj liczbe: ");
+	scanf("%d", &new->amount);
+	printf("Podaj cene: ");
+	scanf("%f", &new->price);
+	printf("Podaj kod kreskowy: ");
+	scanf("%d", &new->bar_code);
+	printf("Podaj nazwe: ");
+	scanf("%s", new->name);
+	int menu;
+	switch (2)
+	{
+	case 1:
+		if (first == NULL)
+		{
+			new->add_order = 1;
+			new->next = NULL;
+			new->prev = NULL;
+			first = new;
+			last = new;
+		}
+		if (a->next == NULL) {
+			new->add_order = last->add_order + 1;
+			new->next = NULL;
+			new->prev = last;
+			last->next = new;
+			last = new;
+		}
+		else
+		{
+			new->add_order = last->add_order + 1;
+
+			a->next->prev = new;
+			new->next = a->next;
+			a->next = new;
+			new->prev = a;
+		}
+		break;
+	case 2:
+		if (first == NULL)
+		{
+			new->add_order = 1;
+			new->next = NULL;
+			new->prev = NULL;
+			first = new;
+			last = new;
+		}
+		if (a->prev == NULL) {
+			new->add_order = last->add_order + 1;
+			new->prev = NULL;
+			a->prev = new;
+			new->next = a;
+			first = new;
+		}
+		else
+		{
+			new->add_order = last->add_order + 1;
+
+			a->prev->next = new;
+			new->prev = a->prev;
+			a->prev = new;
+			new->next = a;
+		}
+		break;
 	}
 }
 void add_file(int amount, float price, long int bar_code, char *name) {
@@ -120,7 +206,7 @@ void add_file(int amount, float price, long int bar_code, char *name) {
 	new->name = name;
 	if (first == NULL)
 	{
-		new->number =  1;
+		new->add_order =  1;
 		new->next = NULL;
 		new->prev = NULL;
 		first = new;
@@ -128,7 +214,7 @@ void add_file(int amount, float price, long int bar_code, char *name) {
 	}
 	else
 	{
-		new->number = last->number + 1;
+		new->add_order = last->add_order + 1;
 		new->next = NULL;
 		new->prev = last;
 		last->next = new;
@@ -140,27 +226,15 @@ void show()
 	Shop *temp;
 	temp = first;
 	if (temp == NULL) {
+		printf("Brak danych");
 		return;
 	}
-	while (temp != NULL)
-	{
-		printf("%d;%d;%f;%d;%s\n",temp->number,temp->amount, temp->price, temp->bar_code, temp->name);
-		temp = temp->next;
-	}
-}
-Shop *show_elem(float data)
-{
-	Shop *temp;
 	temp = first;
 	while (temp != NULL)
 	{
-		if ((data==temp->amount)|| (data == temp->price)|| (data == temp->number)|| (data == temp->bar_code))
-		{
-			return temp;
-		}
+		printf("%d;%d;%d;%f;%d;%s\n",temp->number, temp->add_order,temp->amount, temp->price, temp->bar_code, temp->name);
 		temp = temp->next;
 	}
-	return NULL;
 }
 void save() {
 	int i, j;
@@ -248,7 +322,10 @@ void delete_one(Shop *temp)
 		return;
 	}
 	if ((temp == first) && (temp == last)) {
+		free(temp->name);
 		free(temp);
+		first = NULL;
+		last = NULL;
 		return;
 	}
 	if (temp == first) {
@@ -309,28 +386,133 @@ void show_one() {
 		}
 	} while (opcja != 4);
 }
-void sort(Shop *first)
+void order_number()
 {
-	Shop *newfirst = NULL;
-	Shop *max, *popmax, *tmp;
-	while (first)
-	{
-		max = first;
-		popmax = NULL;
-		tmp = first;
-		while (tmp->next)
-		{
-			if (tmp->next->price > max->price)
-			{
-				popmax = tmp;
-				max = tmp->next;
-			}
-			tmp = tmp->next;
-		}
-		if (popmax) popmax->next = max->next;
-		else first = max->next;
-		max->next = newfirst;
-		newfirst = max;
+	Shop *temp;
+	temp = first;
+	int a = 1;
+	if (temp == NULL) {
+		return;
 	}
-	first = newfirst;
+	while (temp != NULL)
+	{
+	
+		temp->number=a;
+		temp = temp->next;
+		a++;
+	}
+}
+void order_change() {
+	float a;
+	int i, j;
+	a = last->number;
+	for (j = 0; j <= last->number + (a - 4); j++) {
+		for (i = 1; i <= last->number; i++)
+		{
+			swap(show_elem(a - j - i));
+		}
+	}
+}
+void delete_all() {
+	Shop *temp;
+	temp = first;
+	if (temp == NULL) {
+		printf("Brak danych");
+		return;
+	}
+	while (temp->next != NULL)
+	{
+		free(temp->name);
+		temp = temp->next;
+		free(temp->prev);
+	}
+}
+
+Shop *show_elem(float data)
+{
+	Shop *temp;
+	temp = first;
+	while (temp != NULL)
+	{
+		if ((data == temp->number))
+		{
+			return temp;
+		}
+		temp = temp->next;
+	}
+	return NULL;
+}
+Shop *show_char(char *data)
+{
+	Shop *temp;
+	temp = first;
+
+	while (temp != NULL)
+	{
+		if (strcmp(temp->name, data) == 0)
+		{
+			return temp;
+		}
+		temp = temp->next;
+	}
+	return NULL;
+}
+
+void show_series(float data)
+{
+	Shop *temp;
+	temp = first;
+	while (temp != NULL)
+	{
+		if ((data == temp->amount) || (data == temp->price) || (data == temp->number) || (data == temp->bar_code))
+		{
+			printf("%d;%d;%f;%d;%s;%d\n", temp->add_order, temp->amount, temp->price, temp->bar_code, temp->name, temp->number);
+		}
+		temp = temp->next;
+	}
+	return NULL;
+}
+void delete_things(Shop *a, Shop *b) {
+	if (a->number < b->number) 
+	{
+			b->next->prev = a->prev;
+			a->prev->next = b->next;
+	}	
+	if (a->number > b->number) {
+
+		a->next->prev = b->prev;
+		b->prev->next = a->next;
+	}
+}
+void swap(Shop *temp)
+{
+	if (temp == first)//jesli jest pierwszym elmentem
+	{
+		temp->next->next->prev = temp;//trzeci element juz wskazuje na pierwszy ktory stanie sie drugim
+		temp->prev = temp->next;
+		temp->next = temp->next->next;
+		temp->prev->next = temp;
+		temp->prev->prev = NULL;
+		first = temp->prev;
+	}
+	else if(temp->next==last)//jesli jest ostatnim elementem
+    {
+        temp->next->prev=temp->prev;
+        temp->prev->next =temp->next;
+        temp->prev =temp->next;
+        temp->prev->next =temp;
+        temp->next =NULL;
+		last =temp;
+
+    }
+	else//jesli jest gdzie ze srodka
+	{
+		temp->prev->next = temp->next;
+		temp->next->next->prev = temp;
+		temp->next->prev = temp->prev;
+		temp->prev = temp->next;
+		temp->next = temp->next->next;
+		temp->prev->next = temp;
+
+	}
 }
